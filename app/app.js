b97d9a6f088225f9df45cbbd246d30714adc40c9
Emily Kwak <emilykwak1@gmail.com>
2019-02-13 14:08:29 -0800

    snack container - dblclick

--- app/app.js
+++ app/app.js
@@ -18,13 +18,14 @@ $(document).ready(function(){
   var snackData = JSON.parse(localStorage.getItem('snacks')) 
 
   // SNACK SUBMIT BUTTON
-  $('.btn-snack').on('click', function(e){ 
+  $('.btn-snack').on('click', function(e){
     e.preventDefault();
     // snack value
     var snackValue = $('.input-snack').val(); 
     // add new value to snack array
     snacks.push(snackValue); 
     localStorage.setItem('snacks', JSON.stringify(snacks));
+    // var displayText = `'snacks | ${localStorage.getItem('snacks')}`;
     $('.list-snacks').append(`<li>${snackValue}</liv>`);
     $('.input-snack').val('');
 
@@ -40,46 +41,55 @@ $(document).ready(function(){
         }
     }
   });
+
+
+
+ $('.snack-container').dblclick(function(){
+     alert( "Item deleted" );
+  });
+
+
+
   
-
-
-
-
-//   $('.btn-add').on('click', function(e){
-//     console.log(e);
-//     var keyData = $('.input-key').val();
-//     var valueData = $('.input-value').val();
-//     // write to db
-//     localStorage.setItem(keyData, valueData);
-//     // read from db
-//     var displayText = keyData + ' | ' + localStorage.getItem(keyData);
-//     // this only displays the last one? might want to switch to html
-//     // and append a div
-//     // <div class="display-data-item" data-keyValue="keyData">valueData</div>
-//     // if you use backticks ` you can use ${templateLiterals}
-//     // TODO make this vars make sense across the app
-//     $('.container-data').html('<div class="display-data-item" data-keyValue="'+ keyData +'">'+valueData+'</div>');
-//     $('.input-key').val('');
-//     $('.input-value').val('');
-//   });
-
-
-//   // update db
-//     // need to expand when  more than 1 item is added
-
-//   // delete item
-//   $('.container-data').on('click', '.display-data-item', function(e){
-//     console.log(e.currentTarget.dataset.keyvalue);
-//     var keyData = e.currentTarget.dataset.keyvalue;
-//     localStorage.removeItem(keyData);
-//     $('.container-data').text('');
-//   });
-//   // delete all?
-//   $('.btn-clear').click(function(){
-//     localStorage.clear();
-//     $('.container-data').text('');
-//   });
-
-//   $('container-data').toArray(snacks)
-
 // });
+
+
+
+  $('.btn-add').on('click', function(e){
+    console.log(e);
+    var keyData = $('.input-key').val();
+    var valueData = $('.input-value').val();
+    // write to db
+    localStorage.setItem(keyData, valueData);
+    // read from db
+    var displayText = keyData + ' | ' + localStorage.getItem(keyData);
+    // this only displays the last one? might want to switch to html
+    // and append a div
+    // <div class="display-data-item" data-keyValue="keyData">valueData</div>
+    // if you use backticks ` you can use ${templateLiterals}
+    // TODO make this vars make sense across the app
+    $('.container-data').html('<div class="display-data-item" data-keyValue="'+ keyData +'">'+valueData+'</div>');
+    $('.input-key').val('');
+    $('.input-value').val('');
+  });
+
+
+  // update db
+    // need to expand when  more than 1 item is added
+
+  // delete item
+  $('.container-data').on('click', '.display-data-item', function(e){
+    console.log(e.currentTarget.dataset.keyvalue);
+    var keyData = e.currentTarget.dataset.keyvalue;
+    localStorage.removeItem(keyData);
+    $('.container-data').text('');
+  });
+  // delete all?
+  $('.btn-clear').click(function(){
+    localStorage.clear();
+    $('.container-data').text('');
+  });
+
+  $('container-data').toArray(snacks)
+
+});
