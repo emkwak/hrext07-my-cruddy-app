b7a90885d600cab52e7fbfba1c6d0d8a6cdf2bfa
Emily Kwak <emilykwak1@gmail.com>
2019-02-12 20:33:19 -0800

    updated snack / comment out some codes

--- app/app.js
+++ app/app.js
@@ -8,17 +8,16 @@ interact with localstorage
 $(document).ready(function(){
   // this is where we jquery
   //var keyData = 'ourKey'; // going to need to make this dynamic?
-
-  // SNACK LIST
+ 
+  //SNACK
   var snacks = localStorage.setItem('snacks', JSON.stringify(snacks)) ? JSON.parse(localStorage.getItem('snacks')) : ['Brownie Crisps', 'Cookie butter', 'Dolmas','Sweet Plantain Chips', 'Popcorn With Herbs and Spices', 'Roasted Coconut Chips', 'Seasoned Kale Chips'];
   var snackList = $('.list-snacks');
   var snackParent = snackList.parent();
-  var snackContainer = $('.snack-container');
 
   localStorage.setItem('snacks', JSON.stringify(snacks)); 
   var snackData = JSON.parse(localStorage.getItem('snacks')) 
 
-
+  // SNACK SUBMIT BUTTON
   $('.btn-snack').on('click', function(e){ 
     e.preventDefault();
     // snack value
@@ -32,8 +31,8 @@ $(document).ready(function(){
   });
 
 
-
-  var existingSnacks = snackList.detach().each(function(index){ //.detach() method is the same as .remove(), except that .detach() keeps all jQuery data associated with the removed elements. This method is useful when removed elements are to be reinserted into the DOM at a later time.
+  // ITERATE - EXISTING SNACKS 
+  snackList.detach().each(function(index){ //.detach() method is the same as .remove(), except that .detach() keeps all jQuery data associated with the removed elements. This method is useful when removed elements are to be reinserted into the DOM at a later time.
     for (var i = 0; i < snacks.length; i++){
         $(this).append(`<li>${snacks[i]}</li>`);
           if (i === snacks.length - 1){
@@ -46,41 +45,41 @@ $(document).ready(function(){
 
 
 
-  $('.btn-add').on('click', function(e){
-    console.log(e);
-    var keyData = $('.input-key').val();
-    var valueData = $('.input-value').val();
-    // write to db
-    localStorage.setItem(keyData, valueData);
-    // read from db
-    var displayText = keyData + ' | ' + localStorage.getItem(keyData);
-    // this only displays the last one? might want to switch to html
-    // and append a div
-    // <div class="display-data-item" data-keyValue="keyData">valueData</div>
-    // if you use backticks ` you can use ${templateLiterals}
-    // TODO make this vars make sense across the app
-    $('.container-data').html('<div class="display-data-item" data-keyValue="'+ keyData +'">'+valueData+'</div>');
-    $('.input-key').val('');
-    $('.input-value').val('');
-  });
+//   $('.btn-add').on('click', function(e){
+//     console.log(e);
+//     var keyData = $('.input-key').val();
+//     var valueData = $('.input-value').val();
+//     // write to db
+//     localStorage.setItem(keyData, valueData);
+//     // read from db
+//     var displayText = keyData + ' | ' + localStorage.getItem(keyData);
+//     // this only displays the last one? might want to switch to html
+//     // and append a div
+//     // <div class="display-data-item" data-keyValue="keyData">valueData</div>
+//     // if you use backticks ` you can use ${templateLiterals}
+//     // TODO make this vars make sense across the app
+//     $('.container-data').html('<div class="display-data-item" data-keyValue="'+ keyData +'">'+valueData+'</div>');
+//     $('.input-key').val('');
+//     $('.input-value').val('');
+//   });
 
 
-  // update db
-    // need to expand when  more than 1 item is added
+//   // update db
+//     // need to expand when  more than 1 item is added
 
-  // delete item
-  $('.container-data').on('click', '.display-data-item', function(e){
-    console.log(e.currentTarget.dataset.keyvalue);
-    var keyData = e.currentTarget.dataset.keyvalue;
-    localStorage.removeItem(keyData);
-    $('.container-data').text('');
-  });
-  // delete all?
-  $('.btn-clear').click(function(){
-    localStorage.clear();
-    $('.container-data').text('');
-  });
+//   // delete item
+//   $('.container-data').on('click', '.display-data-item', function(e){
+//     console.log(e.currentTarget.dataset.keyvalue);
+//     var keyData = e.currentTarget.dataset.keyvalue;
+//     localStorage.removeItem(keyData);
+//     $('.container-data').text('');
+//   });
+//   // delete all?
+//   $('.btn-clear').click(function(){
+//     localStorage.clear();
+//     $('.container-data').text('');
+//   });
 
-  $('container-data').toArray(snacks)
+//   $('container-data').toArray(snacks)
 
-});
+// });
